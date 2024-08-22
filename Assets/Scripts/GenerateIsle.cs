0c5497b335a1b75a9218164945f622a15a42dc16
LeoKeiji <leo.keiji@gmail.com>
2024-08-22 09:53:28 -0300

    SOS: Fixed GenerateIsle script

--- Assets/Scripts/GenerateIsle.cs
+++ Assets/Scripts/GenerateIsle.cs
@@ -2,35 +2,49 @@ public int numberOfMountains = 3;
 public int mountainSizeRequirement = 2;
 public int numberOfTrees = 10;
 public int treeSizeRequirement = 1;
-public GameObject mountainPrefab;
-public GameObject treePrefab;
+public GameObject MountainPrefap;
+public GameObject TreePrefap;
 
 void GenerateIsland()
 {
     // Existing code for island generation...
 
     // Place mountains
-    PlaceAssets(islandBlocks, mountainPrefab, numberOfMountains, mountainSizeRequirement);
+    if (!PlaceAssets(islandBlocks, MountainPrefap, numberOfMountains, mountainSizeRequirement))
+    {
+        Debug.Log("Not enough space for all mountains. Placed as many as possible.");
+    }
 
     // Place trees
-    PlaceAssets(islandBlocks, treePrefab, numberOfTrees, treeSizeRequirement);
+    if (!PlaceAssets(islandBlocks, TreePrefap, numberOfTrees, treeSizeRequirement))
+    {
+        Debug.Log("Not enough space for all trees. Placed as many as possible.");
+    }
 }
 
-void PlaceAssets(HashSet<Vector3> islandBlocks, GameObject assetPrefab, int numberOfAssets, int sizeRequirement)
+bool PlaceAssets(HashSet<Vector3> islandBlocks, GameObject assetPrefab, int numberOfAssets, int sizeRequirement)
 {
-    int placedAssets = 0;
-    while (placedAssets < numberOfAssets)
+    List<Vector3> validPositions = new List<Vector3>();
+
+    // Find all valid positions for asset placement
+    foreach (Vector3 block in islandBlocks)
     {
-        foreach (Vector3 block in islandBlocks)
+        if (CheckFreeSpace(block, sizeRequirement, islandBlocks))
         {
-            if (CheckFreeSpace(block, sizeRequirement, islandBlocks))
-            {
-                Instantiate(assetPrefab, new Vector3(block.x, islandHeight, block.z), Quaternion.identity);
-                placedAssets++;
-                if (placedAssets >= numberOfAssets) break;
-            }
+            validPositions.Add(block);
         }
     }
+
+    // Place as many assets as possible
+    int assetsToPlace = Mathf.Min(numberOfAssets, validPositions.Count);
+    for (int i = 0; i < assetsToPlace; i++)
+    {
+        Vector3 position = validPositions[i];
+        Instantiate(assetPrefab, new Vector3(position.x, islandHeight, position.z), Quaternion.identity);
+    }
+
+    // Return true if all assets were placed, otherwise return false
+    return assetsToPlace == numberOfAssets;
 }
 
 bool CheckFreeSpace(Vector3 startBlock, int size, HashSet<Vector3> islandBlocks)
